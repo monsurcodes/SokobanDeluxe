36b5981ef481d99114ee5832547dcb6a5fb79f23
monsurcodes <monsurcodes@gmail.com>
2025-09-04 21:41:18 +0530

    Refactor game level handling and add display for level completion

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import pygame
 import os
 import sys
+import time
 from pytmx.util_pygame import load_pygame
 
-from settings import FPS, DISPLAY_RESOLUTION
+from settings import FPS, DISPLAY_RESOLUTION, WIDTH, HEIGHT
 from player import Player
 from boxes import Box, CollisionBox
 
@@ -13,7 +14,6 @@ class Game:
     def __init__(self):
         # general setup
         pygame.init()
-        pygame.display.set_caption("Sokoban Deluxe")
         self.screen = pygame.display.set_mode(DISPLAY_RESOLUTION)
         self.clock = pygame.time.Clock()
         self.running = True
@@ -30,11 +30,32 @@ class Game:
         self.box_marker_points = []
         
         self.setup_map()
+        
+    def unload_map(self):
+        self.all_sprites.empty()
+        self.box_sprites.empty()
+        self.collision_sprites.empty()
+        self.box_marker_points.clear()
+        
+    def display_text(self, text, size, color, position):
+        self.screen.fill("black")
+        font = pygame.font.Font(os.path.join("assets", "fonts", "font.ttf"), size)
+        text = font.render(text, True, color)
+        text_rect = text.get_rect(center=position)
+        self.screen.blit(text, text_rect)
+        pygame.display.update()
 
     def setup_map(self):
         if not os.path.exists(os.path.join("levels", f"{self.game_level}_level.tmx")):
-            print("No more levels available. You've completed the game!")
+            self.unload_map()
+
+            # levels completed screen
+            self.display_text("You have completed all levels!", 32, "White", (WIDTH // 2, HEIGHT // 2))
+            save_game(1) # reset to level 1
+            
+            time.sleep(2) # wait for player to read message
             self.running = False
+            
             pygame.quit()
             sys.exit()
             
@@ -87,12 +108,15 @@ class Game:
             
             # check level completion
             if self.player.level_completed:
-                print(f"Level {self.game_level} completed!")
-                self.all_sprites.empty()
-                self.box_sprites.empty()
-                self.collision_sprites.empty()
-                self.box_marker_points.clear()
+                self.unload_map()
+                
+                # game over screen
+                self.display_text("Level Completed!", 32, "White", (WIDTH // 2, HEIGHT // 2))
+
+                time.sleep(2) # wait for player to read message
                 self.running = False
+                
+                # increment level
                 save_game(self.game_level + 1)
 
             # update screen
