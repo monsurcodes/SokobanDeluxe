6f5017a9597ce4f41d5bdd84ffcbe8fea191d265
monsurcodes <monsurcodes@gmail.com>
2025-09-04 20:24:19 +0530

    Add graphics and tileset for game assets

---
+++ assets/tsx/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="../graphics/tiles.png" width="192" height="64"/>
+</tileset>
